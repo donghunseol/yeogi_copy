8c3a742eb8ae3fb8dff86074549b2425a2398696
김지훈 <greenpig4b@gmail.com>
2024-04-24 00:37:43 +0900

    머스테치페이지

--- src/main/java/com/example/final_project/admin/AdminController.java
+++ src/main/java/com/example/final_project/admin/AdminController.java
@@ -3,10 +3,13 @@ package com.example.final_project.admin;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class AdminController {
     private final AdminService adminService;
     private final HttpSession session;
+
+
 }
