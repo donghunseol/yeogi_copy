51e3629cb136a12e8a11523dcd651ff0cb73f663
greenpig4b <greenpig4b@naver.com>
2024-04-24 16:24:43 +0900

    ã이벤트 더미 생성중

--- src/main/java/com/example/final_project/event/Event.java
+++ src/main/java/com/example/final_project/event/Event.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
@@ -25,26 +26,27 @@ public class Event {
     @Column(nullable = false)
     private String name; // 이벤트 이름
 
-    @Column(nullable = false)
+    // @Column(nullable = false) // 나중에 false로 수정
     private String imageName; // 이벤트 이미지
 
-    @Column(nullable = false)
+    // @Column(nullable = false) // 나중에 false로 수정
     private String imagePath; // 이벤트 이미지 경로
 
     @Column(nullable = false)
-    private LocalDateTime start; // 이벤트 시작일
+    private LocalDate start; // 이벤트 시작일
 
     @Column(nullable = false)
-    private LocalDateTime end; // 이벤트 종료일
+    private LocalDate end; // 이벤트 종료일
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private EventEnum state; // 이벤트 상태 / Enable 진행 중, Disable 종료
 
     @Column(nullable = false)
-    private String createdAt; // 이벤트 이름
+    private String createdAt; // 작성일
 
     @Builder
-    public Event(Integer id, Admin admin, String name, String imageName, String imagePath, LocalDateTime start, LocalDateTime end, EventEnum state, String createdAt) {
+    public Event(Integer id, Admin admin, String name, String imageName, String imagePath, LocalDate start, LocalDate end, EventEnum state, String createdAt) {
         this.id = id;
         this.admin = admin;
         this.name = name;
