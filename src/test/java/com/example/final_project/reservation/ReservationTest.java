72d4d4140e4adfa65f8d9e8aaf3108424c8ca360
minmeanmin <wed1604@naver.com>
2024-04-26 11:28:59 +0900

    temp

--- src/test/java/com/example/final_project/reservation/ReservationTest.java
+++ src/test/java/com/example/final_project/reservation/ReservationTest.java
@@ -1,9 +1,11 @@
 package com.example.final_project.reservation;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @DataJpaTest
@@ -13,32 +15,29 @@ public class ReservationTest {
     ReservationRepository reservationRepository;
 
     @Test
-    public void findaAll_test() {
-            // given
+    public void findAll_test() {
+        // given
 
-            // when
+        // when
         List<Reservation> reservaiton = reservationRepository.findAll();
-            // then
+
+        // then
         System.out.println(reservaiton.size());
-        }
+    }
 
     @Test
     public void findByUserId_test() {
-            // given
-            Integer userId = 1;
-            // when
-            List<Reservation> reservationList = reservationRepository.findByUserId(userId);
-            // then
-            System.out.println("결과========================="+reservationList.size());
-        }
-
-    @Test
-    public void findByUserIdd_test() {
         // given
         Integer userId = 1;
+
         // when
-        List<Reservation> reservationList = reservationRepository.findByUserIdd(userId);
+        List<Reservation> reservationList = reservationRepository.findByUserId(userId);
+
+        // eye
+        System.out.println("findByUserId_test : " + reservationList.size());
+
         // then
-        System.out.println("결과========================="+reservationList.size());
-        }
+        Assertions.assertThat(reservationList.getFirst().getCheckInDate()).isEqualTo(LocalDate.of(2023, 12, 31));
+    }
+
 }
