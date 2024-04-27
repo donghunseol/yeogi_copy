07e13e56fb40a1e8e2bf607ccc8145fd2ed47a96
seoldonghun <ehdgns5647@gmail.com>
2024-04-27 15:14:03 +0900

    예약 등록 완료

--- src/main/java/com/example/final_project/reservation/ReservationService.java
+++ src/main/java/com/example/final_project/reservation/ReservationService.java
@@ -1,7 +1,9 @@
 package com.example.final_project.reservation;
 
+import com.example.final_project._core.enums.PayEnum;
 import com.example.final_project._core.errors.exception.Exception401;
 import com.example.final_project._core.errors.exception.Exception404;
+import com.example.final_project.pay.Pay;
 import com.example.final_project.pay.PayRepository;
 import com.example.final_project.room.Room;
 import com.example.final_project.room.RoomRepository;
@@ -11,6 +13,10 @@ import com.example.final_project.user.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -23,19 +29,41 @@ public class ReservationService {
 
     // 예약 하기
     @Transactional
-    public ReservationResponse.DTO save(ReservationRequest.DTO reqDTO, SessionUser sessionUser, Integer roomId) {
+    public ReservationResponse.DTO makeReservation(ReservationRequest.DTO reqDTO, SessionUser sessionUser, Integer roomId) {
         User user = userRepository.findById(sessionUser.getId())
                 .orElseThrow(() -> new Exception404("존재 하지 않는 계정입니다"));
         Room room = roomRepository.findById(roomId)
                 .orElseThrow(() -> new Exception404("존재 하지 않는 방입니다"));
 
+        // 겹치는 예약 날짜 확인
+        System.out.println("예약 입실 일자 : " + reqDTO.getCheckInDate());
+        System.out.println("예약 퇴실 일자 : " + reqDTO.getCheckOutDate());
+        Optional<List<Reservation>> reservationsOP = reservationRepository.findReservationsByDateRangeAndRoomId(roomId, reqDTO.getCheckInDate(), reqDTO.getCheckOutDate());
+        System.out.println(reservationsOP.get().size());
+
+        if (reservationsOP.get().size() != 0) {
+            throw new Exception401("이미 예약된 날짜 입니다");
+        }
+
         Reservation reservation = reservationRepository.save(reqDTO.toEntity(user, room));
+
+        // 예약 시 결제 안된 상태로 결제 데이터 생성
+        Pay pay = Pay.builder()
+                .reservation(reservation)
+                .amount(0)
+                .way("No payment")
+                .state(PayEnum.PROCESSING)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        payRepository.save(pay);
+
         return new ReservationResponse.DTO(reservation);
     }
 
     // 예약 수정 하기
     @Transactional
-    public ReservationResponse.DTO update(ReservationRequest.UpdateDTO reqDTO, SessionUser sessionUser, Integer reservationId) {
+    public ReservationResponse.DTO modifyReservation(ReservationRequest.UpdateDTO reqDTO, SessionUser sessionUser, Integer reservationId) {
         Reservation reservation = reservationRepository.findById(reservationId)
                 .orElseThrow(() -> new Exception404("존재 하지 않는 예약입니다"));
 
