7370b88395c47f5748c9509d4d9aa3cd8756c9d2
minmeanmin <wed1604@naver.com>
2024-04-29 17:41:37 +0900

    temp

--- src/test/java/com/example/final_project/report/ReportRepositoryTest.java
+++ src/test/java/com/example/final_project/report/ReportRepositoryTest.java
@@ -1,11 +1,7 @@
 package com.example.final_project.report;
 
-
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import java.util.List;
 
 @DataJpaTest
 public class ReportRepositoryTest {
@@ -13,19 +9,4 @@ public class ReportRepositoryTest {
     @Autowired
     private ReportRepository reportRepository;
 
-    @Test
-    public void findByCompanyId_test(){
-        // given
-        Integer companyId = 1;
-
-        // when
-        List<Report> reportList = reportRepository.findByCompanyId(companyId);
-
-        // eye
-        System.out.println("findByCompanyId_test size : " + reportList.size());
-
-        // then
-
-
-    }
 }
