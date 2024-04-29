c7488be7b712f9a5352aa0da002374515ae58af9
minmeanmin <wed1604@naver.com>
2024-04-29 21:39:28 +0900

    기업 심사 거절 구현

--- src/main/java/com/example/final_project/_core/enums/CompanyEnum.java
+++ src/main/java/com/example/final_project/_core/enums/CompanyEnum.java
@@ -1,5 +1,6 @@
 package com.example.final_project._core.enums;
 
 public enum CompanyEnum {
-    ACTIVE, PROGRESSING, QUIT, BLACK // 상태 (ACTIVE : 기업 유지, QUIT : 기업 탈퇴, BLACK : 신고 받아서 제한된 기업, PROGRESSING : 승인 진행 중)
+    // 상태 (ACTIVE : 기업 유지, PROGRESSING : 승인 진행 중, QUIT : 기업 탈퇴, BLACK : 신고 받아서 제한된 기업, REJECT : 가입 거절된 기업)
+    ACTIVE, PROGRESSING, QUIT, BLACK, REJECT
 }
