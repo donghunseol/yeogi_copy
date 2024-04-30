c055f290a6e8856cc90cf47d175a0e9e02a515a0
greenpig4b <greenpig4b@naver.com>
2024-04-30 15:30:40 +0900

    로그인 및 회원가입

--- src/main/java/com/example/final_project/company/CompanyController.java
+++ src/main/java/com/example/final_project/company/CompanyController.java
@@ -7,6 +7,9 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
 
@@ -24,4 +27,50 @@ public class CompanyController {
         List<ReservationResponse.ListDTO> respDTO = reservationService.compReservationList(sessionCompany);
         return respDTO;
     }
+
+    // 로그인
+    @PostMapping("/company/login")
+    public String login(CompanyRequest.LoginDTO reqDTO){
+
+        Company company = companyService.login(reqDTO);
+        session.setAttribute("sessionUser",company);
+
+        return "redirect:/company/main";
+    }
+
+    // 회원가입
+    @PostMapping("/company/join")
+    public String joinCompany(CompanyRequest.JoinDTO reqDTO) {
+        Company company = companyService.joinAndLogin(reqDTO);
+        session.setAttribute("sessionUser", company);
+        return "redirect:/company/main";
+    }
+
+    // 로그인 폼
+    @GetMapping("/company")
+    public String loginForm(){
+
+        return "/company/login-form";
+    }
+
+    // 회원가입 폼
+    @GetMapping("/company/join-form")
+    public String join(){
+        return "/company/join-form";
+    }
+
+    // 로그아웃
+    @GetMapping("/logout")
+    public String logout() {
+        session.invalidate();
+        return "redirect:/company";
+    }
+
+    // 숙소관리 메인페이지
+    @GetMapping("/company/main")
+    public String main(){
+
+        return "/company/stay/main";
+    }
+
 }
--- src/main/java/com/example/final_project/company/CompanyService.java
+++ src/main/java/com/example/final_project/company/CompanyService.java
@@ -1,10 +1,17 @@
 package com.example.final_project.company;
 
+import com.example.final_project._core.errors.exception.Exception400;
+import com.example.final_project._core.errors.exception.Exception401;
 import com.example.final_project._core.errors.exception.Exception404;
 import com.example.final_project._core.utils.JwtUtil;
+import com.example.final_project.user.SessionUser;
+import com.example.final_project.user.User;
+import com.example.final_project.user.UserRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,13 +22,39 @@ public class CompanyService {
 
     //로그인
     @Transactional
-    public String login(CompanyRequest.LoginDTO reqDTO){
+    public Company login(CompanyRequest.LoginDTO reqDTO){
         //1. 아이디 체크
         Company sessionUser = companyRepository.findByIdAndPassword(reqDTO.getEmail(),reqDTO.getPassword())
                 .orElseThrow( () -> new Exception404("아이디 및 패스워드가 일치하지않습니다"));
 
-        String jwt = JwtUtil.companyCreate(sessionUser);
+//        String jwt = JwtUtil.companyCreate(sessionUser);
+//        return jwt;
 
-        return jwt;
+        return sessionUser;
     }
+
+//    // 회원가입
+//    @Transactional
+//    public Company join(CompanyRequest.JoinDTO reqDTO){
+//        //회원가입
+//        return companyRepository.save(reqDTO.toEntity());
+//
+//    }
+
+    // 회원가입
+    @Transactional
+    public Company joinAndLogin(CompanyRequest.JoinDTO reqDTO) {
+
+        //회원가입
+        Company joinUser = companyRepository.save(reqDTO.toEntity());
+
+        //로그인
+        Optional<Company> companyOP = Optional.ofNullable(companyRepository.findByEmail(joinUser.getEmail())
+                .orElseThrow(() -> new Exception404("해당 이메일을 찾을 수 없습니다")));
+
+        //로그인
+        return new Company(joinUser);
+
+    }
+
 }
