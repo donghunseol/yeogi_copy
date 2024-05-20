1ff16de90ccbb56157da78a790909a1b75b76856
김지훈 <greenpig4b@gmail.com>
2024-05-20 21:50:24 +0900

    유ì저 유효성 체크

--- src/main/java/com/example/final_project/user/User.java
+++ src/main/java/com/example/final_project/user/User.java
@@ -2,6 +2,9 @@ package com.example.final_project.user;
 
 import com.example.final_project._core.enums.UserEnum;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +23,9 @@ public class User {
     private Integer id; // 유저 번호
 
     @Column(nullable = false, unique = true)
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Pattern(regexp = "^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}",
+            message = "올바르지 않은 이메일 형식입니다.")
     private String email; // 이메일 (로그인 할 때 아이디로 사용)
 
     @Column(nullable = false)
