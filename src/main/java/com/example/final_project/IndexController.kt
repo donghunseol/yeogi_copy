3a218046837017f70d85a8a91272084395d722ab
seoldonghun <ehdgns5647@gmail.com>
2024-05-30 17:37:21 +0900

    temp1

---
+++ src/main/java/com/example/final_project/IndexController.kt
@@ -0,0 +1,10 @@
+package com.example.final_project
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class IndexController {
+
+
+}
